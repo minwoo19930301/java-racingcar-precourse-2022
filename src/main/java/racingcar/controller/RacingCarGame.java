cbf813cebc517d6b8ef3831b47c28a401caa175d
김민우 <rlaalsdn4564@hanmail.net>
2022-10-09 20:02:59 +0900

    feat : draft process of game

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -1,11 +1,33 @@
 package racingcar.controller;
 
+
 /**
  * @author: Minwoo Kim
  * @date 2022/10/09
  */
 public class RacingCarGame {
-    public void start(){
 
+
+    private void createCars(){
+
+    }
+
+    private void setRetryCount(){
+
+    }
+
+    private void proceedMoving(){
+
+    }
+
+    private void getWinners(){
+
+    }
+
+    public void start(){
+        createCars();
+        setRetryCount();
+        proceedMoving();
+        getWinners();
     }
 }
