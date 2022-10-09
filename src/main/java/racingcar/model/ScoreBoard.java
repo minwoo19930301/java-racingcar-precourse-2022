cc64eb3f86c76c9811ca9e4fec68e29b8ec52195
김민우 <rlaalsdn4564@hanmail.net>
2022-10-09 18:57:46 +0900

    feat : ScoreBoard as First Class Collection

---
+++ src/main/java/racingcar/model/ScoreBoard.java
@@ -0,0 +1,32 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author: Minwoo Kim
+ * @date 2022/10/09
+ */
+public class ScoreBoard {
+
+    private final List<RacingCar> racingCarStats = new ArrayList<>();
+
+    public List<RacingCar> getRacingCarStats(){
+        return racingCarStats;
+    }
+
+    public List<String> findWinners() {
+        Map<Integer, List<String>> map = new HashMap<>();
+        int max = 0;
+        for (RacingCar racingCar : racingCarStats) {
+            if (racingCar.getPosition() >= max) {
+                max = racingCar.getPosition();
+                map.putIfAbsent(max, new ArrayList<>());
+                map.get(max).add(racingCar.getName());
+            }
+        }
+        return map.get(max);
+    }
+}
